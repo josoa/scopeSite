1997bcf8c1ff893e1afe8b9957e0ed6ad363f45f
Ashley Jhaveri <ashleyjhaveri@gmail.com>
2016-02-28 15:02:41 -0800

    removed produciton

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp        = require('gulp');
+var fileinclude = require('gulp-file-include');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 
@@ -6,42 +7,56 @@ var sass        = require('gulp-sass');
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
 
-    browserSync.init({
-        server: "./production"
-    });
+	browserSync.init({
+		server: "./production"
+	});
 
-    gulp.watch("development/scss/*.scss", ['sass']);
-    gulp.watch("development/*.html", ['html']);
-    gulp.watch("development/js/*.js", ['js']);
-    browserSync.reload;
+	gulp.watch("development/scss/*.scss", ['sass']);
+	gulp.watch("development/*.html", ['fileinclude']);
+	gulp.watch("development/js/*.js", ['js']);
+	browserSync.reload;
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("development/scss/*.scss")
-        .pipe(sass())
-        .pipe(gulp.dest("production/css"))
-        .pipe(browserSync.stream());
+	return gulp.src("development/scss/*.scss")
+		.pipe(sass())
+		.pipe(gulp.dest("production/css"))
+		.pipe(browserSync.stream());
 });
 
 gulp.task('js', function() {
-    return gulp.src("development/js/*.js")
-        .pipe(gulp.dest("production/js"))
-        .pipe(browserSync.stream());
+	return gulp.src("development/js/*.js")
+		.pipe(gulp.dest("production/js"))
+		.pipe(browserSync.stream());
+});
+
+gulp.task('fonts', function() {
+	return gulp.src("development/fonts/*.*")
+		.pipe(gulp.dest("production/fonts"));
 });
 
 gulp.task('img', function() {
-    return gulp.src("development/img/*.*")
-        .pipe(gulp.dest("production/img"))
-        .pipe(browserSync.stream());
+	return gulp.src("development/img/*.*")
+		.pipe(gulp.dest("production/img"));
 });
 
-gulp.task('html', function() {
-    return gulp.src("development/*.html")
-        .pipe(gulp.dest("production/"))
-        .pipe(browserSync.stream());
+//gulp.task('html', function() {
+//	return gulp.src("development/*.html")
+//		.pipe(gulp.dest("production/"))
+//		.pipe(browserSync.stream());
+//});
+
+gulp.task('fileinclude', function() {
+  gulp.src(['./development/index.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: './development'
+    }))
+    .pipe(gulp.dest('production/'))
+    .pipe(browserSync.stream());
 });
 
 
-gulp.task('default', ['serve']);
-gulp.task('copy', ['js', 'img', 'html']);
+gulp.task('default', ['serve', 'fileinclude', 'copy']);
+gulp.task('copy', ['js', 'img', 'fonts']);
